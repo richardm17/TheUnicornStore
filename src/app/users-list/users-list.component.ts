9874351217b982c19b2d4283b93a14fa867153f3
Richard Magpantay <magpantay.richard@yahoo.com.ph>
2019-06-01 21:46:13 +0800

    The Unicorn Store Test

---
+++ src/app/users-list/users-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { users } from '../users';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css']
+})
+export class UsersListComponent {
+  users = users;
+
+  share() {
+    window.alert('The user has been shared!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/users.ts
@@ -0,0 +1,17 @@
+export const users = [
+  {
+    name: 'Richard',
+    sex: 'Male'
+  },
+  {
+    name: 'Tanner',
+    sex: 'Male'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
