101e3cb8c9551237bb2e63c773653c10f043d72c
Richard Magpantay <magpantay.richard@yahoo.com.ph>
2019-06-09 22:27:36 +0800

    Services and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { ProductAlertComponent } from './product-alert/product-alert.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -17,7 +18,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'users', component: UsersListComponent }
+      { path: 'users', component: UsersListComponent },
+      { path: 'cart', component: CartComponent }
     ])
   ],
   declarations: [
@@ -26,7 +28,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListComponent,
     UsersListComponent,
     ProductAlertComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   bootstrap: [ AppComponent ]
 })
